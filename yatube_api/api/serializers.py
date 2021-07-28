9a71d2bd812857ec9735dceec852076718686e36
Bolatbek <abdukerimovbn@yandex.ru>
2021-07-28 21:04:53 +0300

    Ver. 0.2 2 failed, 20 passed

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,6 +1,12 @@
+#from typing_extensions import Required
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 from rest_framework import serializers
+from rest_framework.validators import UniqueTogetherValidator
 
 from posts.models import Post, Comment, Group, Follow
+
+User = get_user_model()
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -34,10 +40,31 @@ class CommentSerializer(serializers.ModelSerializer):
 
 class FollowSerializer(serializers.ModelSerializer):
     following = serializers.SlugRelatedField(
+        slug_field='username',
+        queryset=User.objects.all()
+    )
+    user = serializers.SlugRelatedField(
         read_only=True,
-        slug_field='username'
+        slug_field='username',
+        default=serializers.CurrentUserDefault()
     )
 
     class Meta:
         fields = '__all__'
         model = Follow
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=('user', 'following',)
+            )
+        ]
+
+    def create(self, validated_data):
+        if 'following' in self.initial_data:
+            return Follow.objects.create(**validated_data)
+
+    def validate(self, data):
+        if (self.context['request'].user == data['following']
+                and self.context['request'].method == 'POST'):
+            raise ValidationError('Нельзя подписаться на самого себя')
+        return data
